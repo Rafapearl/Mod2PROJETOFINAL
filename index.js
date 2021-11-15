83a70949d8cc06361d7fe03d52e1c649f6097976
bernaRocha <mrochabernardo@gmail.com>
2021-11-15 15:58:21 -0200

    domingo - tarde

--- index.js
+++ index.js
@@ -12,40 +12,40 @@ let message = "";
 
 
 app.get("/", (req, res) => {
-    res.render("index", {titulo: 'Catálogo', message, formula}); // Nome do arquivo, o EJS já busca dentro da pasta views.
-  });
+  res.render("index", { titulo: 'Catálogo', message, formula }); // Nome do arquivo, o EJS já busca dentro da pasta views.
+});
 
-  app.get("/criar", (req, res) => {
-    res.render("criar"); // Nome do arquivo, o EJS já busca dentro da pasta views.
-  });
+app.get("/criar", (req, res) => {
+  res.render("criar"); // Nome do arquivo, o EJS já busca dentro da pasta views.
+});
 
-  app.get("/sobre", (req, res) => {
-    res.render("sobre"); // Nome do arquivo, o EJS já busca dentro da pasta views.
-  });
+app.get("/sobre", (req, res) => {
+  res.render("sobre"); // Nome do arquivo, o EJS já busca dentro da pasta views.
+});
 
-  app.get("/artigo", (req, res) => {
-    res.render("artigo"); // Nome do arquivo, o EJS já busca dentro da pasta views.
-  });
-  app.get("/login", (req, res) => {
-    res.render("login"); // Nome do arquivo, o EJS já busca dentro da pasta views.
-  });
-  app.get("/cadastro", (req, res) => {
-    res.render("cadastro"); // Nome do arquivo, o EJS já busca dentro da pasta views.
-  });
+app.get("/artigo", (req, res) => {
+  res.render("artigo"); // Nome do arquivo, o EJS já busca dentro da pasta views.
+});
+app.get("/login", (req, res) => {
+  res.render("login"); // Nome do arquivo, o EJS já busca dentro da pasta views.
+});
+app.get("/cadastro", (req, res) => {
+  res.render("cadastro"); // Nome do arquivo, o EJS já busca dentro da pasta views.
+});
 
 
-  app.post("/subscription", (req, res) => {
-    const { nome, email, publi, titulo, imagem } = req.body;
-    formula.push({ nome, email, publi, titulo, imagem });
+app.post("/subscription", (req, res) => {
+  const { nome, email, publi, titulo, imagem } = req.body;
+  formula.push({ nome, email, publi, titulo, imagem });
 
-    message = `Parabéns ${nome}, o seu post foi publicado com sucesso!`;
-    res.redirect("/");
-  });
+  message = `Parabéns ${nome}, o seu post foi publicado com sucesso!`;
+  res.redirect("/");
+});
 
-  app.get("/artigo/:id", function (req, res) {
-    const id = req.params.id;
-    const form = formula[id];
-    res.render("artigo", { form, formula });
-  });
+app.get("/artigo/:id", function (req, res) {
+  const id = req.params.id;
+  const form = formula[id];
+  res.render("artigo", { form, formula });
+});
 // Adicionando a const port e uma arow function de callback para mostrar no console que o servidor está rodando.
 app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
