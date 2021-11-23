5138bb8c83fb8d18d09f8971a2755cd1f12ff065
Rafael <rafaironm2@gmail.com>
2021-11-22 21:42:56 -0300

    database

--- database/index.js
+++ database/index.js
@@ -21,7 +21,14 @@ const sequelize = new Sequelize(
   process.env.DB_PASS, {
     host:  process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    protocol: 'postgres',     
+       dialectOptions: {         
+         ssl: {             
+           require: true,             
+           rejectUnauthorized: false         
+         }     
+       } 
 });
 
 module.exports = sequelize;
